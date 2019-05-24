daff490a58c0f5e710322f3b6e5c042782cd1ca5
cg <cgallen@pm.me>
2019-05-24 20:41:34 +0800

    bootstrap style

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#submit').addEventListener('click', () => {
-        var buy = Number(document.querySelector('#buy_price').value);
-        var buy_rate = Number(document.querySelector('#buy_rate').value);
-        var sell_rate = Number(document.querySelector('#sell_rate').value);
-
-        if (buy == 0) {
-            document.querySelector('#buy_warning').style.visibility = 'visible';
-        } else if (buy_rate == 0) {
-            document.querySelector('#buy_rate_warning').style.visibility = 'visible';
-        } else if (sell_rate == 0) {
-            document.querySelector('#sell_rate_warning').style.visibility = 'visible';
-        } else {
-            var sell_price = (buy * (1 + buy_rate)) / (1 - sell_rate);
-            
-            // return ans
-            document.querySelector('#balance_price').innerHTML = sell_price;
-        }
-    })
 })
+
+function balance_price() {
+    var buy = Number(document.querySelector('#buy_price').value);
+    var buy_rate = Number(document.querySelector('#buy_rate').value);
+    var sell_rate = Number(document.querySelector('#sell_rate').value);
+
+    if (buy == 0) {
+        document.querySelector('#buy_warning').style.visibility = 'visible';
+    } else if (buy_rate == 0) {
+        document.querySelector('#buy_rate_warning').style.visibility = 'visible';
+    } else if (sell_rate == 0) {
+        document.querySelector('#sell_rate_warning').style.visibility = 'visible';
+    } else {
+        var sell_price = ((buy * (1 + buy_rate)) / (1 - sell_rate)).toFixed(2);
+        
+        // return ans
+        document.querySelector('#balance_price').innerHTML = sell_price;
+    }
+    
+    return false;
+
+}
