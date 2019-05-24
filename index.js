4566594b4fbd4991d0979d83b949f30aad2aa3fb
cg <cgallen@pm.me>
2019-05-24 14:04:43 +0800

    basic website without decorating

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#submit').addEventListener('click', () => {
+        var buy = Number(document.querySelector('#buy_price').value);
+        var buy_rate = Number(document.querySelector('#buy_rate').value);
+        var sell_rate = Number(document.querySelector('#sell_rate').value);
+
+        if (buy == 0) {
+            document.querySelector('#buy_warning').style.visibility = 'visible';
+        } else if (buy_rate == 0) {
+            document.querySelector('#buy_rate_warning').style.visibility = 'visible';
+        } else if (sell_rate == 0) {
+            document.querySelector('#sell_rate_warning').style.visibility = 'visible';
+        } else {
+            var sell_price = (buy * (1 + buy_rate)) / (1 - sell_rate);
+            
+            // return ans
+            document.querySelector('#balance_price').innerHTML = sell_price;
+        }
+    })
+})
