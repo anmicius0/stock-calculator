695064f604b28d2257c0badbc5cef78f39f7f7d0
cg <cgallen@pm.me>
2019-05-26 09:36:31 +0800

    onfocus => clear warning

--- index.js
+++ index.js
@@ -5,20 +5,31 @@ function balance_price() {
     var buy = Number(document.querySelector('#buy_price').value);
     var buy_rate = Number(document.querySelector('#buy_rate').value);
     var sell_rate = Number(document.querySelector('#sell_rate').value);
+    var input = true;
 
     if (buy == 0) {
         document.querySelector('#buy_warning').style.visibility = 'visible';
-    } else if (buy_rate == 0) {
+        input = false
+    } 
+    if (buy_rate == 0) {
         document.querySelector('#buy_rate_warning').style.visibility = 'visible';
-    } else if (sell_rate == 0) {
+        input = false
+    }  
+    if (sell_rate == 0) {
         document.querySelector('#sell_rate_warning').style.visibility = 'visible';
-    } else {
+        input = false
+    }
+    if (input) {
         var sell_price = ((buy * (1 + buy_rate)) / (1 - sell_rate)).toFixed(2);
         
         // return ans
         document.querySelector('#balance_price').innerHTML = sell_price;
     }
-    
+
     return false;
 
+}
+
+function clear_warning(element) {
+    document.querySelector(`#${element.id}`).style.visibility = 'hidden'
 }
